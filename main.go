9976669850f70ead3610218c59a5d0d19617f1b3
David Adrian <davidcadrian@gmail.com>
2023-09-26 20:44:46 -0600

    reload code

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	_ "embed"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -23,10 +24,6 @@ var css string
 var base string
 
 func serve(t *template.Template, data any) {
-	buf := bytes.Buffer{}
-	if err := t.Execute(&buf, data); err != nil {
-		log.Fatalf("unable to render index.md: %s", err)
-	}
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 
@@ -42,10 +39,56 @@ func serve(t *template.Template, data any) {
 		log.Printf("%s", "starting server on localhost:8080")
 		http.ListenAndServe("localhost:8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			log.Printf("%s %s", r.Method, r.URL.Path)
-			w.Write(buf.Bytes())
+			switch r.URL.Path {
+			case "index.html", "", "/", "/index":
+				t, data, err := reload()
+				if err != nil {
+					w.WriteHeader(500)
+					w.Write([]byte(err.Error()))
+					return
+				}
+				if err := t.Execute(w, data); err != nil {
+					log.Printf("error writing template: %s", err)
+				}
+			default:
+				w.WriteHeader(404)
+				return
+			}
 		}))
 	}()
 	wg.Wait()
+}
+
+func reload() (*template.Template, map[string]any, error) {
+	md, err := os.ReadFile("index.md")
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to read index.md: %w", err)
+	}
+	tmpl, err := os.ReadFile("index.html")
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to read index.html: %w", err)
+	}
+	css, err := os.ReadFile("index.css")
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to read index.css: %w", err)
+	}
+	return render(md, string(tmpl), string(css))
+}
+
+func render(md []byte, tmpl, css string) (*template.Template, map[string]any, error) {
+	buf := bytes.Buffer{}
+	if err := goldmark.Convert(md, &buf); err != nil {
+		return nil, nil, fmt.Errorf("unable to render markdown: %w", err)
+	}
+	t, err := template.New("index.html").Parse(base)
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to parse index.html", err)
+	}
+	data := map[string]any{
+		"Content": buf.String(),
+		"Css":     css,
+	}
+	return t, data, nil
 }
 
 func print(t *template.Template, data any) {
@@ -55,19 +98,13 @@ func print(t *template.Template, data any) {
 }
 
 func main() {
-	buf := bytes.Buffer{}
-	if err := goldmark.Convert(md, &buf); err != nil {
-		log.Fatalf("Invalid index.md: %s", err)
-	}
-	t := template.Must(template.New("index.html").Parse(base))
-	data := map[string]any{
-		"Content": buf.String(),
-		"Css":     css,
+	t, data, err := render(md, base, css)
+	if err != nil {
+		log.Fatalf("%s", err)
 	}
 	if len(os.Args) <= 1 {
 		print(t, &data)
 	}
-
 	if len(os.Args) > 1 && os.Args[1] == "serve" {
 		serve(t, &data)
 	}
